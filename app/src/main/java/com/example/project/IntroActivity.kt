38d360cd609782003dcc6975bf00e8ce94a214b2
Koose <KooseTheBlogger@gmail.com>
2022-07-22 15:01:43 +0000

    Day2

--- app/src/main/java/com/example/project/IntroActivity.kt
+++ app/src/main/java/com/example/project/IntroActivity.kt
@@ -1,11 +1,24 @@
 package com.example.project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.Login.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+        }
+        binding.signUp.setOnClickListener {
+            startActivity(Intent(this, Login::class.java))
+        }
     }
 }
